0cc57af7a952494fb79d78b8fd5d01c132c31293
Ivan Goropashnyj <goropashnyj9000@gmail.com>
2025-03-19 05:32:42 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "@/app/ui/global.css";
-import { Inter } from "next/font/google";
+import { inter } from "./ui/fonts";
 
-export const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({
   children,
 }: {
@@ -9,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} `}>{children}</body>
     </html>
   );
 }
